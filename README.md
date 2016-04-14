6e135d1584fcd0f86b03d43d86abd3381345eda1
CodyHulsey <sendcodyamessage@live.com>
2016-04-13 21:30:55 -0700

    I moved logic into funcitons. Move the alerts into the browser level.

---
+++ README.md
@@ -0,0 +1,10 @@
+Class 4 | Assignment 4
+
+Originally created by: Chris
+
+Forked and edited by: Cody
+
+During this assignment:
+- Move login into functions
+- Move the alerts into the browser level
+- linking JS with an HTML file
--- app.js
+++ app.js
@@ -1,88 +1,136 @@
+var pElOne = document.getElementById('answerOne');
+var pElTwo = document.getElementById('answerTwo');
+var pElThree = document.getElementById('answerThree');
+var pElFour = document.getElementById('answerFour');
+
 var guestName = prompt("WTF is your name, mate?");
-alert("Good to meet you, " + guestName);
-console.log("User entered: " + guestName);
 
-var guestAnswer = prompt("Why are you here, " + guestName + "?");
-alert(guestAnswer + ": ORLY? Is that the best you could come up with?");
-console.log("User entered: " + guestAnswer);
+var els = [pElOne, pElTwo, pElThree, pElFour];
+var questions = [
+  'What is my last name?',
+  'What is my favorite animal?',
+  'Where do I work?',
+  'What is my favorite number?',
+];
+var answers = [
+  "harnois",
+  "hyacinth macaw",
+  "code fellows",
+  6,
+];
 
-var counterRight = 0;
-var counterWrong = 0;
-var quest1 = prompt("What is my last name?").toLowerCase();
-
-if(quest1 === "harnois") {
-  console.log("User entered: " + quest1);
-  alert("Congrats, " + guestName + ", you're right! it's Harnois.")
-  counterRight++;
-  console.log("counterRight = " + counterRight);
-} else {
-  console.log("User entered: " + quest1);
-  alert("WRONG, SO. WRONG. DAMNIT " + guestName + "It's actually Harnois.");
-  counterWrong++;
-  console.log("counterWrong = " + counterWrong);;
-}
-
-var quest2 = prompt("What is my favorite animal?").toLowerCase();
-if(quest2 === "hyacinth macaw") {
-  console.log("User entered: " + quest2);
-  alert("Correct. You know me well, " + guestName);
-  counterRight++;
-  console.log("counterRight = " + counterRight);
-} else {
-  console.log("User entered: " + quest2);
-  alert("False. This one is tricky, " + guestName + ". Hyacinth Macaw is the answer.");
-  counterWrong++;
-  console.log("counterWrong = " + counterWrong);;
-}
-
-var quest3 = prompt("Where do I work?").toLowerCase();
-if(quest3 === "code fellows") {
-  console.log("User entered: " + quest3);
-  alert("How could you not know that, " + guestName + "?");
-  counterRight++;
-  console.log("counterRight = " + counterRight);
-} else {
-  console.log("User entered: " + quest3);
-  alert("Seriously? You don't know, " + guestName + "? It's actually Code Fellows.");
-  counterWrong++;
-  console.log("counterWrong = " + counterWrong);;
-}
-
-var counterNum = 0;
-var userGuess = parseInt(prompt("Guess my fave number:"));
-console.log("User entered: " + userGuess);
-
-while(counterNum < 2) {
-  if(userGuess === 6) {
-    alert("You win!...nothing!");
-    counterRight++
-    alert("You guessed " + counterRight + "questions correctly!");
-    alert("You guessed " + counterWrong + "questions incorrectly!");
-    break;
-  } else if (userGuess < 6) {
-    alert("Wrong. Try something higher, " + guestName + ".");
-
-    counterWrong++;
-    counterNum++;
-
-    console.log("counterWrong = " + counterWrong);
-    console.log("counterNum = " + counterNum);
-    var userGuess = parseInt(prompt("Guess my fave number:"));
-  } else if (userGuess > 6) {
-    alert("Wrong. Try something lower, " + guestName + ".");
-
-    counterWrong++;
-    counterNum++;
-
-    console.log("counterWrong = " + counterWrong);
-    console.log("counterNum = " + counterNum);
-
-    var userGuess = parseInt(prompt("Guess my fave number:"));
+function game(questions, answers, els) {
+  var userInput = prompt(questions);
+  if (userInput.toLowerCase() === answers) {
+    els.textContent = "Congrats, " + guestName + ", you're right!";
+  } else if (parseInt(userInput) && isNaN(userInput) === false) {
+    var counter = 0;
+    while (counter <= 2) {
+      if (userInput === 6) {
+        els.textContent = "You win!...Nothing!";
+        break;
+      } else if (userInput < 6) {
+        els.textContent = "Wrong. Try something higher, " + guestName + ".";
+        counter++;
+      } else if (userInput > 6) {
+        els.textContent = "Wrong. Try something lower, " + guestName + ".";
+        counter++;
+      }
+    }
+  } else {
+    els.textContent = "Is that the best you could come up with, " + guestName + "?";
   }
 }
 
-alert("Wrong again. Game over, " + guestName + ". Correct answers: " + counterRight
-+ " Incorrect answer: " + counterWrong);
+for(var i = 0; i < questions.length; i++) {
+  game(questions[i], answers[i], els[i]);
+}
 
-console.log("counterWrong = " + counterWrong);
-console.log("counterNum = " + counterNum);
+// var guestName = prompt("WTF is your name, mate?");
+// alert("Good to meet you, " + guestName);
+// console.log("User entered: " + guestName);
+//
+// var guestAnswer = prompt("Why are you here, " + guestName + "?");
+// alert(guestAnswer + ": ORLY? Is that the best you could come up with?");
+// console.log("User entered: " + guestAnswer);
+//
+// var counterRight = 0;
+// var counterWrong = 0;
+// var quest1 = prompt("What is my last name?").toLowerCase();
+//
+// if(quest1 === "harnois") {
+//   console.log("User entered: " + quest1);
+//   alert("Congrats, " + guestName + ", you're right! it's Harnois.")
+//   counterRight++;
+//   console.log("counterRight = " + counterRight);
+// } else {
+//   console.log("User entered: " + quest1);
+//   alert("WRONG, SO. WRONG. DAMNIT " + guestName + "It's actually Harnois.");
+//   counterWrong++;
+//   console.log("counterWrong = " + counterWrong);;
+// }
+//
+// var quest2 = prompt("What is my favorite animal?").toLowerCase();
+// if(quest2 === "hyacinth macaw") {
+//   console.log("User entered: " + quest2);
+//   alert("Correct. You know me well, " + guestName);
+//   counterRight++;
+//   console.log("counterRight = " + counterRight);
+// } else {
+//   console.log("User entered: " + quest2);
+//   alert("False. This one is tricky, " + guestName + ". Hyacinth Macaw is the answer.");
+//   counterWrong++;
+//   console.log("counterWrong = " + counterWrong);;
+// }
+//
+// var quest3 = prompt("Where do I work?").toLowerCase();
+// if(quest3 === "code fellows") {
+//   console.log("User entered: " + quest3);
+//   alert("How could you not know that, " + guestName + "?");
+//   counterRight++;
+//   console.log("counterRight = " + counterRight);
+// } else {
+//   console.log("User entered: " + quest3);
+//   alert("Seriously? You don't know, " + guestName + "? It's actually Code Fellows.");
+//   counterWrong++;
+//   console.log("counterWrong = " + counterWrong);;
+// }
+//
+// var counterNum = 0;
+// var userGuess = parseInt(prompt("Guess my fave number:"));
+// console.log("User entered: " + userGuess);
+//
+// while(counterNum < 2) {
+//   if(userGuess === 6) {
+//     alert("You win!...nothing!");
+//     counterRight++
+//     alert("You guessed " + counterRight + "questions correctly!");
+//     alert("You guessed " + counterWrong + "questions incorrectly!");
+//     break;
+//   } else if (userGuess < 6) {
+//     alert("Wrong. Try something higher, " + guestName + ".");
+//
+//     counterWrong++;
+//     counterNum++;
+//
+//     console.log("counterWrong = " + counterWrong);
+//     console.log("counterNum = " + counterNum);
+//     var userGuess = parseInt(prompt("Guess my fave number:"));
+//   } else if (userGuess > 6) {
+//     alert("Wrong. Try something lower, " + guestName + ".");
+//
+//     counterWrong++;
+//     counterNum++;
+//
+//     console.log("counterWrong = " + counterWrong);
+//     console.log("counterNum = " + counterNum);
+//
+//     var userGuess = parseInt(prompt("Guess my fave number:"));
+//   }
+// }
+//
+// alert("Wrong again. Game over, " + guestName + ". Correct answers: " + counterRight
+// + " Incorrect answer: " + counterWrong);
+//
+// console.log("counterWrong = " + counterWrong);
+// console.log("counterNum = " + counterNum);
